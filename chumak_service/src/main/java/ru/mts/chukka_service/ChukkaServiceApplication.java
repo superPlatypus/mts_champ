c80736b0c106ce984e7bd9d6d95eed166de83487
Mihail <misha.chumak13@gmail.com>
2024-03-14 15:27:23 +0300

    Добавление шаблона системы доставки

---
+++ chumak_service/src/main/java/ru/mts/chukka_service/ChukkaServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mts.chukka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChukkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChukkaServiceApplication.class, args);
+	}
+
+}
---
+++ chumak_service/src/test/java/ru/mts/chukka_service/ChukkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mts.chukka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChukkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
